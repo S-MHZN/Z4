ed124c5c25164fa6a9230f008f07dc6804a970d5
Maharjan Smaran <S-Maharjan@ad-nex.nankai-express.co.jp>
2026-04-15 15:46:40 +0900

    first commit

--- controllers/sendMessage.js
+++ controllers/sendMessage.js
@@ -14,12 +14,12 @@ exports.handleSendMessage = async (req, res) => {
 
     console.log("EMAIL_USER:", process.env.EMAIL_USER);
     console.log("EMAIL_PASS:", process.env.EMAIL_PASS);
-    
+
     // 1. Create transporter
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
-      secure: false,
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
